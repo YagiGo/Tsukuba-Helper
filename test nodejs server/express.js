391e07b0ad1485a48e34756dcebac2e0a03f59b4
Zhaoxin Wu <jeremywu1995@gmail.com>
2018-03-14 15:42:59 +0900

    "added some site pages"

---
+++ test nodejs server/express.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.engine('pug', require('pug').__express);
+
+app.set('view engine', 'pug');
+
+app.set('views', '/views');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.listen(8080);
+
